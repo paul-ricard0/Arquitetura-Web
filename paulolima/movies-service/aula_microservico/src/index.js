651768ffb11df18fd6d1788223ef728079cfd7fa
paul-ricard0 <pauloricardo4515@gmail.com>
2023-04-19 21:23:57 -0300

    micro_serviços

---
+++ paulolima/movies-service/aula_microservico/src/index.js
@@ -0,0 +1,7 @@
+require("dotenv-safe").config();
+const movies = require("./api/movies");
+const server = require("./server/server");
+const repository = require("./repository/repository");
+server.start(movies, repository, (err, app) => {
+  console.log("just started");
+});
